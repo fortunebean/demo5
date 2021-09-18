9a8b810221f214b378b9b125aff767364603873d
fortunebean <89978842+fortunebean@users.noreply.github.com>
2021-09-18 13:30:44 +0900

    demo 5 초기화 설정-2

--- src/main/java/com/example/demo5/ServletInitializer.java
+++ src/main/java/com/example/demo5/ServletInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-
+//2021 09 18
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Demo5Application.class);
